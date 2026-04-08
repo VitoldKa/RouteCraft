f1d9e84efe3985aca90f3af0902f41b30c8eac33
VitoldKa <vka@tech-easy.ch>
2026-04-08 20:43:58 +0200

    feat(backend): add request logging

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,22 @@
 use actix_files::Files;
 use actix_web::{App, HttpResponse, HttpServer, get, middleware};
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port = 8080;
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     println!("Serving on http://localhost:{}/", port);
+    println!("Static files: ./static -> /");
+    println!("Spatial cache: ./spatial_cache -> /spatial_cache");
 
     HttpServer::new(|| {
         App::new()
             // Serve static files from the ./static directory at /
+            .wrap(middleware::Logger::new(
+                r#"%a "%r" %s %b "%{Referer}i" "%{User-Agent}i" %T"#,
+            ))
             .wrap(middleware::DefaultHeaders::new().add(("Cache-Control", "public, max-age=60")))
             .service(Files::new("/spatial_cache", "./spatial_cache"))
             .service(Files::new("/", "./static").index_file("index.html"))
